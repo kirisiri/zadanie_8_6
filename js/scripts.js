e0cde4baabf8086622c5d7859fd6b27bf27a620c
Kasia Kowalska <k.kowalska68@yahoo.com>
2018-01-07 21:13:48 +0100

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var name = prompt('Enter your name');
+alert('Hello, ' + name)
+alert('I will compute a triangle\'s area for you')
+//console.log('Hello, ' + name);
+var a = prompt('Enter your base');
+var h = prompt('Enter your height');
+var triangleArea = a*h/2; 
+alert(name + ', your triangle\'s area equals: ' + triangleArea)
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
